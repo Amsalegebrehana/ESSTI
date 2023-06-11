2ca0bbe6d0ae62c5042d155ed0e0478e380ce36d
AmsaleG <amsale@kemercode.com>
2023-06-11 20:54:24 +0300

    first commit

---
+++ components/TopDescription.vue
@@ -0,0 +1,199 @@
+<template>
+    <div class="bg-white mt-0 px-16">
+        <div class="col-span-12 grid grid-cols-12 gap-6 mt-2 py-16 px-7">
+            <div class="col-span-12 sm:col-span-6 2xl:col-span-3 intro-y">
+                <div class="relative">
+                    <img class="block w-3/4 rounded-3xl mb-2 ml-3" src="../static/5J0A9108.JPG" alt="Your Company">
+                    <div class="overlay w-3/4"></div>
+                </div>
+            </div>
+            <div class="col-span-12 sm:col-span-6 2xl:col-span-3 intro-y">
+                <p class="text-5xl font-extrabold text-green-400 mt-4 italic">
+                    Who are we?
+                </p>
+                <div class="mt-5 mr-5">
+                    <p class="text-xl mt-5 text-purple-900 font-bold">
+                        The Ethiopian Space Kids Club is a dynamic and engaging organization dedicated to inspiring and educating children about the wonders of space science and exploration. Our mission is to ignite curiosity, foster a passion for learning, and empower young minds to reach for the stars. Through
+
+                    </p>
+                </div>
+                <button> Read More</button>
+        </div>
+                                
+        </div>
+        <hr class="border-t border-purple-800 my-4 mx-16">
+
+        <div class="col-span-12 grid grid-cols-12 gap-6 mt-8">
+            <div class="col-span-12 sm:col-span-4 2xl:col-span-3 intro-y">
+                <p class="text-5xl font-extrabold text-green-400 mt-4 italic">
+                    What We Offer
+                </p>
+                
+            </div>
+            <div class="col-span-12 sm:col-span-8 2xl:col-span-3 intro-y">
+                <div class="col-span-12 md:col-span-6 xl:col-span-4 2xl:col-span-12 mt-3">
+                    <div class="mt-5 relative before:block before:absolute before:w-px before:h-[85%] before:bg-slate-200 before:dark:bg-darkmode-400 before:ml-5 before:mt-5">
+                        <div class="intro-x relative flex items-center ">
+                            <div class="before:block before:absolute before:w-20 before:h-px before:bg-slate-200 before:dark:bg-darkmode-400 before:mt-5 before:ml-5">
+                                <div class="w-10 h-10 flex-none bg-primary rounded-full overflow-hidden flex items-center justify-center border border-red-500  p-10">
+                                    <span class="text-red-600 text-6xl font-extrabold italic">1</span>
+                                </div>
+                            </div>
+                            <div class="box  px-5 py-3 ml-4 flex-1 zoom-in border-l border-gray-300">
+                                <div class="flex items-center ">
+                                    <hr class="border border-gray-500 w-4 mr-2"/>
+                                    <div class="text-4xl text-purple-900 font-extrabold italic">Educational Programs</div>
+                                   
+                                </div>
+                                <div class="text-purple-800 text-xl mt-4 ml-4">Our club offers a comprehensive curriculum that 
+                                    covers various aspects of space science application, Application of remote sensing satellite, 
+                                    Geodesy, UAV, Rocket, Aerospace engineering, Mapping, Coding, Robotics, Astronomy, and space exploration. Through age-appropriate modules and hands-on activities, we provide an enriching 
+                                    learning experience that sparks imagination and encourages critical thinking.</div>
+                            </div>
+                        </div>
+
+                            <!-- Rest of the code -->
+                        <div class="intro-x relative flex items-center ">
+                            <div class="before:block before:absolute before:w-20 before:h-px before:bg-slate-200 before:dark:bg-darkmode-400 before:mt-5 before:ml-5">
+                                <div class="w-10 h-10 flex-none bg-primary rounded-full overflow-hidden flex items-center justify-center border border-red-800  p-10">
+                                    <span class="text-red-600 text-6xl font-extrabold italic">2</span>
+                                </div>
+                            </div>
+                            <div class="box px-5 py-3 ml-4 flex-1 zoom-in border-l border-gray-300">
+                                <div class="flex items-center">
+                                    <hr class="border border-gray-500 w-4 mr-2"/>
+                                    <div class="text-4xl text-purple-900 font-extrabold italic">Club Activities</div>
+                                   
+                                </div>
+                                <div class="text-purple-800 text-xl mt-4 ml-4">Members of the Ethiopian Space Kids Club 
+                                    can participate in regular meetings, workshops, and events. These gatherings feature educational sessions, demonstrations, and presentations by guest speakers, scientists, and professionals in the field. We also organize field trips to observatories, planetariums, 
+                                    and science museums to provide a firsthand experience of space exploration.
+                                </div>
+                            </div>
+                        </div>
+                    <!--     -->
+                    <div class="intro-x relative flex items-center ">
+                            <div class="before:block before:absolute before:w-20 before:h-px before:bg-slate-200 before:dark:bg-darkmode-400 before:mt-5 before:ml-5">
+                                <div class="w-10 h-10 flex-none bg-primary rounded-full overflow-hidden flex items-center justify-center border border-red-800  p-10">
+                                    <span class="text-red-600 text-6xl font-extrabold italic">3</span>
+                                </div>
+                            </div>
+                            <div class="box px-5 py-3 ml-4 flex-1 zoom-in border-l border-gray-300">
+                                <div class="flex items-center">
+                                    <hr class="border border-gray-500 w-4 mr-2"/>
+                                    <div class="text-4xl text-purple-900 font-extrabold italic">Competitions and Projects</div>
+                                   
+                                </div>
+                                <div class="text-purple-800 text-xl mt-4 ml-4">
+                                    We encourage creativity, problem-solving, and teamwork through space- 
+                                    related competitions and projects. Members can showcase their skills and 
+                                    innovative ideas in science fairs, model-making contests, robotics challenges, and more. Our dedicated mentors provide guidance and support to help members 
+                                    excel in their projects and participate in regional and national competitions.
+                                </div>
+                            </div>
+                    </div>
+                    <div class="intro-x relative flex items-center ">
+                        <div class="before:block before:absolute before:w-20 before:h-px before:bg-slate-200 before:dark:bg-darkmode-400 before:mt-5 before:ml-5">
+                            <div class="w-10 h-10 flex-none bg-primary rounded-full overflow-hidden flex items-center justify-center border border-red-800  p-10">
+                                <span class="text-red-600 text-6xl font-extrabold italic">4</span>
+                            </div>
+                        </div>
+                        <div class="box px-5 py-3 ml-4 flex-1 zoom-in border-l border-gray-300">
+                            <div class="flex items-center">
+                                <hr class="border border-gray-500 w-4 mr-2"/>
+                                <div class="text-4xl text-purple-900 font-extrabold italic">Community Engagement</div>
+                                
+                            </div>
+                            <div class="text-purple-800 text-xl mt-4 ml-4">The Ethiopian Space Kids Club actively engages with schools, communities, 
+                                and local organizations to spread awareness and inspire more children to pursue space science. 
+                                We conduct outreach programs, workshops, and presentations to share our knowledge and 
+                                passion with a wider audience. Through collaborations and partnerships, 
+                                we aim to create a strong network of space enthusiasts and promote the importance of STEM education.
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                </div>
+
+            </div>
+                                
+        </div>
+        <hr class="border-t border-purple-800 my-7 mx-16">
+        <!--  -->
+        <div class="flex justify-center mb-5">
+            <div class="max-w-2xl">
+                <div class="px-10">
+                    <h2 class="text-red-600 text-5xl text-center font-extrabold italic mb-5">Online Learning Platform</h2>
+                    <p class="text-red-500 text-center">We are excited to offer our members an online education platform,
+                    designed to enhance their learning experience and provide access to resources from anywhere.</p>
+                </div>
+            </div>
+        </div>
+        <!--  -->
+        <div class="flex justify-center mt-5 p-4">
+            <div class="border border-green-300 rounded-3xl mr-5 p-4 m-2 hover:shadow-xl  hover:-translate-y-0.5  hover:rotate-2 hover:scale-105 transition-all duration-300">
+                <h3 class="text-xl font-bold text-center text-green-400">Interactive Lessons</h3>
+                <!-- call images from static folder -->
+                <img src="../static/icons-17.png" alt="Online Learning Platform" class="text-center p-10">
+            </div>
+            <div class="border border-red-700 rounded-3xl mr-5 p-4 m-2 hover:shadow-xl hover:-translate-y-0.5 hover:rotate-2 hover:scale-105 transition-all duration-300">
+                <h3 class="text-xl font-bold text-center text-red-700">Discussion Forums</h3>
+                <img src="../static/icons-18.png" alt="Online Learning Platform" class="text-center p-10">
+            </div>
+            <div class="border border-purple-800 rounded-3xl mr-5 p-4 m-2 hover:shadow-xl hover:-translate-y-0.5 hover:rotate-2 hover:scale-105 transition-all duration-300">
+                <h3 class="text-xl font-bold text-center text-purple-900">Resource Library</h3>
+                <img src="../static/icons-19.png" alt="Online Learning Platform" class="text-center p-10">
+            </div>
+            <div class="border border-red-400 rounded-3xl mr-5 p-4 m-2 hover:shadow-xl hover:-translate-y-0.5 hover:rotate-2 hover:scale-105 transition-all duration-300">
+                <h3 class="text-xl font-bold text-center text-red-500">Project Collaboration</h3>
+                <img src="../static/icons-20.png" alt="Online Learning Platform" class="text-center p-10">
+            </div>
+        </div>
+
+
+
+        <hr class="border-t border-red-500 my-4 mx-48 mt-10 ">
+        <!--  -->
+        <div class="flex justify-center mt-5 mb-10">
+    <div class="max-w-3xl">
+        <div class="px-6">
+            <h2 class=" text-3xl text-center text-green-400 font-extrabold mb-5 italic">Registration</h2>
+            <p class=" text-center text-green-400 text-lg">If you're interested in joining the Ethiopian Space Kids Club, we invite you to fill out our online registration form. Simply provide the required information, including your name, age, contact details, and a brief statement about your interest in space science. 
+                We will review your application and get back to you with further instructions.
+            </p>
+            <div class="flex justify-center mt-10 mb-5"> 
+                <button class="border border-gray-300 text-4xl font-bold rounded-md italic px-8 py-2 bg-yellow-400">
+                 Register
+                </button>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+    </div>
+</template>
+
+<style scoped>
+
+.card1text{
+    color: #5fa67a !important;
+}
+.relative {
+  position: relative;
+}
+
+.overlay {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 80%;
+  height: 100%;
+  border: 2px solid rgb(102, 223, 102);
+  border-radius: 50px;
+  transform: rotate(-19deg);
+  z-index: 1;
+}
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="all">
+
+    <NavBar/>
+    <TopDescription/>
+    <div class="col-span-12 grid grid-cols-12 gap-6 mt-8 px-16">
+      <div class="col-span-12 sm:col-span-6 2xl:col-span-3 intro-y">
+              <h2 class="text-yellow-300 text-4xl font-bold italic px-16"> Support Us</h2>
+              <div class="mt-10">
+                  <p class="text-yellow-300 text-lg text-center">
+                    SSGI as a Government organization, the Ethiopian Space Kids Club relies on the support of government budget, generous individuals, sponsors, and partners. If you believe in our mission and would like to contribute, there are various ways to support us. You can make a donation, volunteer your time and expertise, or explore sponsorship opportunities.
+
+                  </p>
+                  <p class="text-yellow-300 mt-5 text-lg text-center">
+                   
+Together, we can inspire the next generation of space enthusiasts and empower them to reach for the stars.
+                  </p>
+              </div>
+             
+          </div>
+          <div class="col-span-12 sm:col-span-6 2xl:col-span-3 intro-y">
+            <div class="flex justify-center mt-7 mb-5 py-16"> 
+                <button class="border border-gray-300 text-purple-900 text-4xl font-bold rounded-md italic px-8 py-2 bg-yellow-400">
+              Donate
+                </button>
+            </div>
+          </div>
+        
+                                
+        </div>
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: 'IndexPage'
+}
+</script>
+
+<style>
+body{
+  background-color: rgb(93, 11, 93) !important;
+}
+
+</style>
