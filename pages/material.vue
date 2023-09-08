8c45ccd46b768a702c38ef5de7e4c14e3712cc68
alazaradane <alazargithub369@gmail.com>
2023-09-08 16:02:36 +0300

    Last Commit

--- pages/material.vue
+++ pages/material.vue
@@ -41,16 +41,69 @@
     <form class="mb-6">
         <div class="py-2 px-4 mb-4 bg-white rounded-lg rounded-t-lg border border-gray-200 dark:bg-gray-800 dark:border-gray-700">
             <label for="comment" class="sr-only">Your comment</label>
-            <textarea id="comment" rows="6" v-model="comment"
+            <textarea id="comment" rows="6"  v-model="comment"
                 class="px-0 w-full text-sm text-gray-900 border-0 focus:ring-0 focus:outline-none dark:text-white dark:placeholder-gray-400 dark:bg-gray-800"
                 placeholder="Write a comment..." required></textarea>
         </div>
-        <button @click="postcomment()"
+        <button @click.prevent="postcomment()"
             class="inline-flex items-center py-2.5 px-4 text-xs font-medium text-center text-white bg-blue-700 rounded-lg focus:ring-4 focus:ring-primary-200 dark:focus:ring-primary-900 hover:bg-primary-800">
-            Post comment
+            Post Discussion
         </button>
     </form>
-    <article class="p-6 mb-6 text-base bg-white rounded-lg dark:bg-gray-900">
+
+    <article v-for="(comment, index) in comments" class="p-6 mb-6 text-base bg-white rounded-lg dark:bg-gray-900">
+        <footer class="flex justify-between items-center mb-2">
+            <div class="flex items-center">
+                <p class="inline-flex items-center mr-3 text-sm text-gray-900 dark:text-white"><img
+                        class="mr-2 w-6 h-6 rounded-full"
+                        src="https://flowbite.com/docs/images/people/profile-picture-2.jpg"
+                        alt="Michael Gough">{{fullName}}</p>
+                <p class="text-sm text-gray-600 dark:text-gray-400"><time pubdate datetime="2022-02-08"
+                        title="February 8th, 2022">{{formattedDate}}</time></p>
+            </div>
+            <button id="dropdownComment1Button" data-dropdown-toggle="dropdownComment1"
+                class="inline-flex items-center p-2 text-sm font-medium text-center text-gray-400 bg-white rounded-lg hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-50 dark:bg-gray-900 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+                type="button">
+                <svg class="w-5 h-5" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20"
+                    xmlns="http://www.w3.org/2000/svg">
+                    <path
+                        d="M6 10a2 2 0 11-4 0 2 2 0 014 0zM12 10a2 2 0 11-4 0 2 2 0 014 0zM16 12a2 2 0 100-4 2 2 0 000 4z">
+                    </path>
+                </svg>
+                <span class="sr-only">Comment settings</span>
+            </button>
+            <!-- Dropdown menu -->
+            <div id="dropdownComment1"
+                class="hidden z-10 w-36 bg-white rounded divide-y divide-gray-100 shadow dark:bg-gray-700 dark:divide-gray-600">
+                <ul class="py-1 text-sm text-gray-700 dark:text-gray-200"
+                    aria-labelledby="dropdownMenuIconHorizontalButton">
+                    <li>
+                        <a href="#"
+                            class="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Edit</a>
+                    </li>
+                    <li>
+                        <a href="#"
+                            class="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Remove</a>
+                    </li>
+                    <li>
+                        <a href="#"
+                            class="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Report</a>
+                    </li>
+                </ul>
+            </div>
+        </footer>
+        <p class="text-gray-500 dark:text-gray-400"> {{comment.text}}</p>
+       
+        <div class="flex items-center mt-4 space-x-4">
+            <button type="button"
+                class="flex items-center text-sm text-gray-500 hover:underline dark:text-gray-400">
+                <svg aria-hidden="true" class="mr-1 w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z"></path></svg>
+                Reply
+            </button>
+        </div>
+    </article>
+
+    <article  class="p-6 mb-6 text-base bg-white rounded-lg dark:bg-gray-900">
         <footer class="flex justify-between items-center mb-2">
             <div class="flex items-center">
                 <p class="inline-flex items-center mr-3 text-sm text-gray-900 dark:text-white"><img
@@ -91,7 +144,7 @@
                 </ul>
             </div>
         </footer>
-        <p class="text-gray-500 dark:text-gray-400">{{comment.value}}</p>
+      
         <p class="text-gray-500 dark:text-gray-400">I want to be an astronomer when I grow up. I'm not sure if that means writing books, or teaching a class, or working in an observatory, but I just want to be able to learn about the universe and then teach it to other people. I'm not sure if I'll actually be able to DO that,
          but I'll just have to see how life goes and learn what I can for now. Thanks for this Crash Course!</p>
         <div class="flex items-center mt-4 space-x-4">
@@ -102,6 +155,9 @@
             </button>
         </div>
     </article>
+    
+
+
     <article class="p-6 mb-6 ml-6 lg:ml-12 text-base bg-white rounded-lg dark:bg-gray-900">
         <footer class="flex justify-between items-center mb-2">
             <div class="flex items-center">
@@ -285,12 +341,40 @@
 import { ref } from 'vue'
 // import { Dialog, DialogPanel, DialogTitle, TransitionChild, TransitionRoot } from '@headlessui/vue'
 // import { XMarkIcon } from '@heroicons/vue/24/outline'
-var comment = ref('')
-const open = ref(true)
+const fullName = 'Alazer Adane';
 
-const postcomment=()=>{
-    console.log(comment.value)
-}
+const currentDate = new Date();
+
+
+const months = [
+  "Jan.", "Feb.", "Mar.", "Apr.", "May", "Jun.",
+  "Jul.", "Aug.", "Sept.", "Oct.", "Nov.", "Dec."
+];
+
+const month = months[currentDate.getMonth()];
+const day = currentDate.getDate();
+const year = currentDate.getFullYear();
+
+const formattedDate = `${month} ${day}, ${year}`;
+
+console.log(formattedDate); 
+
+const comments = ref([
+ 
+]);
+const open = ref(true);
+
+const postcomment = () => {
+  if (comment.value.trim() !== '') {
+    comments.value.push({
+      
+      text: comment.value,
+      
+    });
+   console.log(comment.value);
+   
+  }
+};
 </script>
   
 <style scoped>
